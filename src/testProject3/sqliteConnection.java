b1253ee08220031440c0395619f5cdf0d498d949
Domnica <caraveleadomnica@gmail.com>
2017-03-10 17:21:13 +0200

    Revert "Revert "bug fixes""
    This reverts commit f44cccce9844bed94eec35bb0e4a0cc10f86c7ce.

--- src/testProject3/sqliteConnection.java
+++ src/testProject3/sqliteConnection.java
@@ -9,9 +9,9 @@ public class sqliteConnection {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			//Conexiune windows
-			//Connection con = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\StefanDomnica\\Documents\\GitHub\\testProject3\\poli.sqlite");
+			Connection con = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\StefanDomnica\\Documents\\GitHub\\testProject3\\poli.sqlite");
 			//Conexiune linux
-			Connection con = DriverManager.getConnection("jdbc:sqlite:/home/dom/workspace/testProject3/poli.sqlite");
+			//Connection con = DriverManager.getConnection("jdbc:sqlite:/home/dom/workspace/testProject3/poli.sqlite");
 			JOptionPane.showMessageDialog(null, "Connection succesfull to database");
 			return con;
 		
